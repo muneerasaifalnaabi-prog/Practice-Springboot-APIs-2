1e9ecd5cedbb98c4e0b2a9eedb91fd5d86b688a5
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-03 11:23:07 +0400

    Added constructor for fields

--- tr.app/src/main/java/com/example/tr/app/Author.java
+++ tr.app/src/main/java/com/example/tr/app/Author.java
@@ -4,4 +4,10 @@ public class Author {
     private  Integer id;
     private String name;
     private String biography;
+
+    public Author(Integer id, String name, String biography) {
+        this.id = id;
+        this.name = name;
+        this.biography = biography;
+    }
 }
