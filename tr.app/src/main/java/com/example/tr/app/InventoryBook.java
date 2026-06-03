da88bc552cad26578cf1ae68dc1737d2ffabbc3a
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-03 14:19:43 +0400

    created setter and getter

--- tr.app/src/main/java/com/example/tr/app/InventoryBook.java
+++ tr.app/src/main/java/com/example/tr/app/InventoryBook.java
@@ -13,4 +13,35 @@ public class InventoryBook {
         this.stockCount = stockCount;
     }
 
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Integer getStockCount() {
+        return stockCount;
+    }
+
+    public void setStockCount(Integer stockCount) {
+        this.stockCount = stockCount;
+    }
 }
