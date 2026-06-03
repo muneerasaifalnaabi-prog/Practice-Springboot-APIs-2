51640e1690b298ad334e46944971ddf688a5c35e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-03 11:23:36 +0400

    Added getter and setter

--- tr.app/src/main/java/com/example/tr/app/Author.java
+++ tr.app/src/main/java/com/example/tr/app/Author.java
@@ -10,4 +10,28 @@ public class Author {
         this.name = name;
         this.biography = biography;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
 }
