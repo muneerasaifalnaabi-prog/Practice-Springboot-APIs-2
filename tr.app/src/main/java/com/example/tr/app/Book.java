8e6661cd21f32e30d1121c7132a6be8651a7a60f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-03 09:33:38 +0400

    added constructor

--- tr.app/src/main/java/com/example/tr/app/Book.java
+++ tr.app/src/main/java/com/example/tr/app/Book.java
@@ -3,4 +3,9 @@ package com.example.tr.app;
 public class Book {
     private  Integer id ;
     private String name;
+
+    public Book(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
