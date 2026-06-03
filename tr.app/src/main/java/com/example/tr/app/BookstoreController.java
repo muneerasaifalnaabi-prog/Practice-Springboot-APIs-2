ea830a719decff5ef2a372def2b8d47a736fb3d7
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-03 14:27:07 +0400

    Initialize a static in-memory List<InventoryBook> to store the catalog

--- tr.app/src/main/java/com/example/tr/app/BookstoreController.java
+++ tr.app/src/main/java/com/example/tr/app/BookstoreController.java
@@ -1,4 +1,11 @@
 package com.example.tr.app;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class BookstoreController {
+    public  List<InventoryBook> inventoryBooks = new ArrayList<>();
 }
