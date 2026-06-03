1324244680f46024ca8c8062adcae3c25c666e06
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-03 14:32:42 +0400

    Added addInventoryBook method

--- tr.app/src/main/java/com/example/tr/app/BookstoreController.java
+++ tr.app/src/main/java/com/example/tr/app/BookstoreController.java
@@ -1,5 +1,7 @@
 package com.example.tr.app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -7,5 +9,10 @@ import java.util.List;
 
 @RestController
 public class BookstoreController {
-    public  List<InventoryBook> inventoryBooks = new ArrayList<>();
+    public List<InventoryBook> inventoryBooks = new ArrayList<>();
+
+    @GetMapping("/addInventoryBokk")
+    public String addInventoryBokk(@RequestParam Integer id, @RequestParam String title, @RequestParam Double price, @RequestParam Integer stock) {
+           InventoryBook inventoryBook =new InventoryBook(id,title,price,stock);
+    }
 }
