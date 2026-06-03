fc0b5e55f6ef6a7f0c013e53f0f53c5cc9af2905
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-03 09:43:31 +0400

    Added mthod addbook that request 2 parameter id & name

--- tr.app/src/main/java/com/example/tr/app/BookController.java
+++ tr.app/src/main/java/com/example/tr/app/BookController.java
@@ -1,5 +1,7 @@
 package com.example.tr.app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,5 +10,9 @@ import java.util.List;
 @RestController
 public class BookController {
     private static List<Book> books = new ArrayList<>();
+    @GetMapping("/add-book")
+    public String addBook(@RequestParam Integer id,@RequestParam String name){
+
+    }
 
 }
