a3a9959edb69a2a9340325a634aba982d5f8b558
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-03 11:35:24 +0400

    added method addauthor

--- tr.app/src/main/java/com/example/tr/app/LibraryController.java
+++ tr.app/src/main/java/com/example/tr/app/LibraryController.java
@@ -1,5 +1,6 @@
 package com.example.tr.app;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,6 +9,14 @@ import java.util.List;
 @RestController
 public class LibraryController {
     private static List<Author> authorList=new ArrayList<>();
-    (private static List<Book> bookList =new ArrayList<>();
+    private static List<Book> bookList =new ArrayList<>();
+
+public String addAuthor(@RequestParam Integer id ,@RequestParam String name,@RequestParam String biography){
+    Author author = new Author(id,name,biography);
+    authorList.add(author);
+    return "Author added successfully";
+
+}
+
 
 }
