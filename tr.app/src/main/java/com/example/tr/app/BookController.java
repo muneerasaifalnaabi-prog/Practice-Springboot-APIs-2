f6bc4f89a2f0dc994956de812f860630685c467f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-03 09:38:10 +0400

    Added list hold object for Book class

--- tr.app/src/main/java/com/example/tr/app/BookController.java
+++ tr.app/src/main/java/com/example/tr/app/BookController.java
@@ -2,6 +2,11 @@ package com.example.tr.app;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class BookController {
+    private static List<Book> books = new ArrayList<>();
+
 }
