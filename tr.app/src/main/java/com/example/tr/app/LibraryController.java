3d99ff606a0fcdb35a3c49456c7538b08e8915a8
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-03 11:29:29 +0400

    declare 2 list for Author&Book

--- tr.app/src/main/java/com/example/tr/app/LibraryController.java
+++ tr.app/src/main/java/com/example/tr/app/LibraryController.java
@@ -2,6 +2,11 @@ package com.example.tr.app;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class LibraryController {
+    private static List<Author> authorList=new ArrayList<>();
+
 }
